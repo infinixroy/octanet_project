1839eeece44ea31581f58ac839483ede5ab6b23e
infinixroy <168994842+infinixroy@users.noreply.github.com>
2024-05-06 01:14:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//handle navbar
+
+//button "Have a Look"
+
+//fitness texts
+
+//prices section
+
+//slider portion for testimonials
+const tnslider = tns({
+    container: ".slider",
+    slideBy: 1,
+    speed: 700,
+    nav: true,
+    navPoisition: "bottom",
+    autoPlay: true,
+    autoPlayTimeOut: 7000,
+    autoPlayButtonOutput: false,
+    controlsContainer: "#controls",
+    prevButton: ".previous",
+    nextButton: ".next"
+})
